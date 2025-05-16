06dc0caa6e8865232e21341e0989cb4c4342f252
aakashstha1 <aakash.cerestha90@gmail.com>
2025-05-16 14:03:36 +0545

    Add pages

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-gray-100 dark:bg-gray-700 py-4 mt-auto flex bottom-0">
+      <div className="max-w-7xl mx-auto flex flex-col items-center gap-1 ">
+        <p className="text-gray-600 dark:text-gray-300">&copy; {new Date().getFullYear()} E-Learning Platform</p>
+        <p className="font-semibold text-black/80 dark:text-white">All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+function Home() {
+  return <div>Home</div>;
+}
+
+export default Home;
